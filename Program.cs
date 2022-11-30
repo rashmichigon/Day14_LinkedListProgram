e5346b1f4d59fdbe43cf4c4071f8a3fc4ffd4ea4
Rashmi <rashmichigon65@gmail.com>
2022-11-30 09:59:55 +0530

    Day14_LinkedListProgram

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day14_LinkedListProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> L = new LinkedList<int>();
+            L.AddFirst(56);
+            L.AddFirst(30);
+            L.AddFirst(70);
+           
+            Console.Write("Linked List elements are: ");
+            foreach (int i in L)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
